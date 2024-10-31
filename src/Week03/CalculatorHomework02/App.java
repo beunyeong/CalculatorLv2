57375b1eabf08ffbecf6cc2f6c0b200bf68ff501
beunyeong <oo1409@kakao.com>
2024-10-31 11:27:44 +0900

    feat : Calculator Project Lv2

--- src/Week03/CalculatorHomework02/App.java
+++ src/Week03/CalculatorHomework02/App.java
@@ -2,13 +2,13 @@ package Week03.CalculatorHomework02;
 
 import java.util.Scanner;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
 
-        // 객체를 인스턴스화
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
-        String exit;
+        String exit;  // 끝내는 변수
+        String choiceremove;  //진행 여부 확인 변수
 
         do {
             System.out.println("첫번째 숫자를 입력하세요");
@@ -20,21 +20,30 @@ public class Main {
             System.out.println("두번째 숫자를 입력하세요");
             int secondNumber = scanner.nextInt();
 
-            double result = calculator.calculate(firstNumber,operator,secondNumber);
-            System.out.println(result);
-            calculator.setResult(result);
+            System.out.println(calculator.calculate(firstNumber,operator,secondNumber));
+            calculator.getResultList();  //저장된 데이터 출력
 
-            System.out.println("종료를 원하면 'exit'를 입력하세요 / 계속하려면 아무 키나 입력하세요");
+            scanner.nextLine();   //버퍼비우기
+
+
+            //저장된 결과를 확인하고 삭제하는 반복문
+            while (true) {
+                System.out.print("이전 결과를 삭제하시겠습니까? / remove 입력 시 삭제 : ");
+                choiceremove = scanner.nextLine();
+                if (choiceremove.equals("remove")) {
+                    calculator.getResultList();  //이후의 저장된 연산 결과 출력
+                    calculator.setRemoveResult();  //첫 번째 데이터를 삭제
+                } else {
+                    break;
+                }
+            }
+
+            System.out.println("종료를 원하면 'exit'를 입력하세요 / 계속하려면 아무 키나 입력하세요 : ");
             scanner.nextLine();
             exit = scanner.nextLine();
 
         } while (!"exit".equals(exit));
 
-
-//        List<Double> resultList = calculator.getResult();
-//        for(Double result : resultList) {
-//            System.out.println(result);
-//        }
 
     }
 }
--- src/Week03/CalculatorHomework02/Calculator.java
+++ src/Week03/CalculatorHomework02/Calculator.java
@@ -4,40 +4,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-    private List<Double> result = new ArrayList<>();
+
+    // 연산 결과를 저장하는 resultList() , 외부에서 접근 할 수 없도록 private 선언
+    private List<Double> resultList = new ArrayList<>();
 
     public Double calculate(int firstNumber, char operator, int secondNumber) {
-
-        Double result = 0.0;
-
         switch (operator) {
             case '+':
-                result = (double)firstNumber + secondNumber;
+                resultList.add((double)(firstNumber + secondNumber));
                 break;
             case '-':
-                result = (double)firstNumber - secondNumber;
+                resultList.add((double)(firstNumber - secondNumber));
                 break;
             case '*':
-                result = (double)firstNumber * secondNumber;
+                resultList.add((double)(firstNumber * secondNumber));
                 break;
             case '/':
                 if (secondNumber == 0) {
                     System.out.println("오류!: 0으로 나눌 수 없습니다.");
                 } else {
-                    result = (double)firstNumber / secondNumber;
+                    resultList.add((double)(firstNumber / secondNumber));
                     break;
                 }
             default:
                 System.out.println("오류!: 잘못된 연산자를 입력 했습니다.");
         }
-        return result;
+        return resultList.get(resultList.size()-1);
     }
-    // Getter 메서드
-    public List<Double> getResult(){
-        return result;
+
+    //결과를 조회하는 메서드 : getter
+    public void getResultList() {
+        if (resultList.isEmpty()) {
+            System.out.println("현재 계산 결과가 없습니다.");
+        } else {
+            System.out.println("현재 저장된 데이터");
+            for (int i = 0; i < resultList.size(); i++) {
+                System.out.println((i + 1) + "번째 연산 결과 : " + resultList.get(i));
+
+            }
+        }
     }
-    // Setter 메서드
-    public void setResult(Double result){
-        this.result.add(result);
+
+    // 저장된 이전 결과를 삭제하는 메서드 : Setter
+    public void setRemoveResult(){
+        if (resultList.isEmpty()) {
+            System.out.print("");
+        } else {
+            System.out.println("삭제되었습니다.");
+            resultList.removeFirst();
+        }
     }
+
 }
